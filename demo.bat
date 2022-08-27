b979c5c49077b2fe4440abcc0a174b90ca59222e
LJY-RS <ljy_whu_2012@whu.edu.cn>
2022-08-27 12:13:46 +0800

    upload

---
+++ demo.bat
@@ -0,0 +1,3 @@
+::使用方法：LNIFT.exe   输入影像1   输入影像2   是否存在旋转（0否1是）  输出配准变换参数   输出匹配点
+LNIFT.exe  ./data/2.TIF ./data/1.TIF 0 ./res/transformation.txt  ./res/matches.txt 
+::LNIFT.exe  ./data/pair227_1.png ./data/pair227_2.png 1 ./res/transformation.txt  ./res/matches.txt 
